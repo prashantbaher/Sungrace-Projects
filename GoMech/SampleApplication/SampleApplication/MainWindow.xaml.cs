fc349d5b421969da3298a62e9978e324a3b3cd7c
Prashant Baher <prashantbaher@gmail.com>
2019-01-22 19:15:45 +0530

    update gomech for new UI

--- GoMech/SampleApplication/SampleApplication/MainWindow.xaml.cs
+++ GoMech/SampleApplication/SampleApplication/MainWindow.xaml.cs
@@ -30,6 +30,9 @@ namespace SampleApplication
         public MainWindow()
         {
             InitializeComponent();
+
+            // Loads this function when application start
+            StartupMethods();
         }
 
         #endregion
@@ -37,12 +40,26 @@ namespace SampleApplication
         #region Private Methods
 
         /// <summary>
+        /// Loads these things when application start
+        /// </summary>
+        public void StartupMethods()
+        {
+            // Getting directory information of Example folder
+            DirectoryInfo outputDirectoryInfo = new DirectoryInfo(@"C:\ProgramData\Hypertherm CAM\ProNest 2019\Examples\");
+
+            // Assigning the dxf file to cad part list 
+            CADPartList.ItemsSource = outputDirectoryInfo.GetFiles("*.dxf"); 
+        }
+
+        /// <summary>
         /// Run Autonest function in ProNest
         /// </summary>
         /// <param name="sender">Button itself is the sender</param>
         /// <param name="e">Event arguments for this button</param>
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void Button_Click(object sender, RoutedEventArgs e)
         {
+            SungraceApplication.Visibility = Visibility.Hidden;
+
             // Object for our ProNest application
             IpnApplication pronestObject;
 
@@ -55,7 +72,8 @@ namespace SampleApplication
             // Creating instance of ProNest application
             pronestObject = (IpnApplication)Activator.CreateInstance(pronestType);
 
-            pronestObject.Visible = true;
+            // Making ProNest visible for user
+            pronestObject.Visible = PronestAppVisibility.IsChecked.Value ? true : false;
 
             // Getting CAD properties of imported part
             IpnCADImportProperties CADImportProperties = pronestObject.Job.PartList.GetCADImportProperties();
@@ -64,26 +82,34 @@ namespace SampleApplication
             IpnNestingProperties NestingProperties = pronestObject.Job.PartList.GetNestingProperties();
 
             // Setting number of nest required
-            //int nestQuantity = 0;
             NestingProperties.QuantityRequired = int.Parse(NestQuantityTextBox.Text);
 
             // Setting material of importing cad file
-            NestingProperties.Material = pronestObject.Job.Machine.Materials.GetMaterialByID(18);
+            // NestingProperties.Material = pronestObject.Job.Machine.Materials.GetMaterialByID(18);
+            int selectedMaterial = int.Parse((string)MaterialList.SelectedValue);
+            NestingProperties.Material = pronestObject.Job.Machine.Materials.GetMaterialByID(selectedMaterial);
+
 
             // Re-applying the leads
             NestingProperties.RetainAllExistingLeads = false;
 
             // Setting class of part to "40Amp N2/N2" before importing cad file
-            NestingProperties.PartClass = "40Amp N2/N2";
+            NestingProperties.PartClass = MaterialList.Text;
 
-            // Path to our sample dxf file
-            string CADfilePath = @"C:\ProgramData\Hypertherm CAM\ProNest 2019\Examples\Base Plate.dxf";
+            // Path to our sample dxf files
+            // string CADfilePath = @"C:\ProgramData\Hypertherm CAM\ProNest 2019\Examples\Base Plate.dxf";
+            string CADfilePath = CADPartList.SelectedValue.ToString();
 
             // Adding CAD file to partlist
             pronestObject.Job.PartList.AddCADFile(CADfilePath, NestingProperties, CADImportProperties);
 
             // Getting default setup values for before starting autonest
             IpnAutoNestSetupValues AutoNestSetupValues = pronestObject.Job.AutoNest.GetSetupValues();
+
+            // Assigning cutome plate width and length
+            // NOTE: ProNest use Length first then width
+            AutoNestSetupValues.CustomPlateWidth = 5;
+            AutoNestSetupValues.CustomPlateLength = 3;
 
             // Starting autonest
             pronestObject.Job.AutoNest.Start(AutoNestSetupValues, false, false);
@@ -94,40 +120,107 @@ namespace SampleApplication
             // Setting default output extension
             string outputExtension = pronestObject.Job.Machine.Settings.ReadString("NC Parameters", "default_cnc_extension", "CNC");
 
+            // Getting output job name and assigning it to project job name
+            pronestObject.Job.Name = OutputJobNameTextBox.Text;
+
+            // Setting Folder name of output job
+            string outputFileFolderName = outputFolder + pronestObject.Job.Name;
+
+            // Getting output foldername information
+            DirectoryInfo directoryInfo = new DirectoryInfo(outputFileFolderName);
+
+            // If output folder name did not exist, then create a new one with the output folder name
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+
             // Setting output index [Right now I don't know why we use it]
             int OutputIndex = 1;
 
             // Looping through each nest in the list of all nest done in our job
             foreach (IpnNest nest in pronestObject.Job.Nests)
             {
-                // Getting output job name and assigning it to project job name
-                pronestObject.Job.Name = OutputJobNameTextBox.Text;
-
-                // Setting Folder name of output job
-                string outputFileFolderName = outputFolder + pronestObject.Job.Name;
-
-                // Getting output foldername information
-                DirectoryInfo directoryInfo = new DirectoryInfo(outputFileFolderName);
-
-                // If output folder name did not exist, then create a new one with the output folder name
-                if (!directoryInfo.Exists)
-                {
-                    directoryInfo.Create();
-                }
-
                 // Output every nest
                 nest.Output(outputFileFolderName + "\\" + pronestObject.Job.Name + "_" + OutputIndex.ToString() + "." + outputExtension, 0, false, true);
 
+                nest.ExportDXF(outputFileFolderName + "\\" + pronestObject.Job.Name + "_" + OutputIndex.ToString() + "." + "DXF");
+
                 // Increasing the index by 1
                 OutputIndex++;
+                
             }
+
+            // Saving this job
+            pronestObject.Job.SaveAs(outputFileFolderName + "\\" + pronestObject.Job.Name + ".nif", null);
+
+            // Output message to user
+            MessageBox.Show("Job Successfully Completed", "Sungrace Application", MessageBoxButton.OK);
+
+            // Visible our application after hitting Ok button
+            SungraceApplication.Visibility = Visibility.Visible;
 
             // Quitting the ProNest after completing job
             // pronestObject.Quit();
+        }
 
+        #region Up and Down Arrow Buttons
+
+        /// <summary>
+        /// Increment the quantity of required nest
+        /// </summary>
+        /// <param name="sender">Button itself is the sender</param>
+        /// <param name="e">Event arguments for this button</param>
+        private void UpArrowButton(object sender, RoutedEventArgs e)
+        {
+            // Getting nest quantity text and storing into integer
+            int nestQuantity = int.Parse(NestQuantityTextBox.Text);
+
+            // Increasing the nest quantity by 1
+            ++nestQuantity;
+
+            // Setting the incremental quantity inplace of original text
+            NestQuantityTextBox.Text = nestQuantity.ToString();
+        }
+
+        /// <summary>
+        /// Decrement the quantity of required nest
+        /// </summary>
+        /// <param name="sender">Button itself is the sender</param>
+        /// <param name="e">Event arguments for this button</param>
+        private void DownArrowButton(object sender, RoutedEventArgs e)
+        {
+            // Getting nest quantity text and storing into integer
+            int nestQuantity = int.Parse(NestQuantityTextBox.Text);
+
+            // Decresing the nest quantity by 1
+            --nestQuantity;
+
+            // If nest quantity is less than 0 do not decrement
+            if (!(nestQuantity <= 0))
+            {
+                // Setting the Decremental quantity inplace of original text
+                NestQuantityTextBox.Text = nestQuantity.ToString();
+            }
 
         }
 
+
         #endregion
+
+        #endregion
+
+        /*
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string[] StagedFiles = Directory.GetFiles(@"C:\ProNest");
+
+            foreach (string file in StagedFiles)
+            {
+
+                textBox1.Text += file + Environment.NewLine;
+            }
+        }
+        */
     }
 }
